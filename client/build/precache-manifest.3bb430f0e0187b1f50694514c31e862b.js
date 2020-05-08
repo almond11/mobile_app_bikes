4bd7309c42935d57a1b9e2cb5833f4eee47aebd7
almond11 <disearch11@gmail.com>
2020-05-07 21:08:25 -0700

    init

---
+++ client/build/precache-manifest.3bb430f0e0187b1f50694514c31e862b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2a4240de8c7a0b1cd42",
+    "url": "/static/css/main.dae43041.chunk.css"
+  },
+  {
+    "revision": "f2a4240de8c7a0b1cd42",
+    "url": "/static/js/main.3b675852.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "153242236ec563e67d46",
+    "url": "/static/js/2.8c15e647.chunk.js"
+  },
+  {
+    "revision": "a2410ed69c5547e4d69e06a4b8f3bbe9",
+    "url": "/index.html"
+  }
+];
